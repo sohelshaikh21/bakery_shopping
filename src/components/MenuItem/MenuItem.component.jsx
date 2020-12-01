68f650f0e87cedf3c385566325f371f5b0d7d06f
Sohel Shaikh <47694897+sohelshaikh21@users.noreply.github.com>
2020-12-01 12:29:51 +0530

    added links https

--- src/components/MenuItem/MenuItem.component.jsx
+++ src/components/MenuItem/MenuItem.component.jsx
@@ -1,20 +1,24 @@
 import React from "react";
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import "./MenuItem.style.css";
 const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
-    return (
-        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
-            <div style={
-                {
-                    backgroundImage: `url(${imageUrl})`
-                }
-            } className="menu-background"></div>
-            <div className="content">
-                <h2>{title}</h2>
-                <span>Shop Now</span>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+        className="menu-background"
+      ></div>
+      <div className="content">
+        <h2>{title}</h2>
+        <span>Shop Now</span>
+      </div>
+    </div>
+  );
 };
 
 export default withRouter(MenuItem);
--- src/pages/HomePage/HomePage.component.jsx
+++ src/pages/HomePage/HomePage.component.jsx
@@ -2,51 +2,56 @@ import React from "react";
 import Menu from "../../components/Menu/Menu.component";
 
 class HomePage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            menus: [
-                {
-                    id: 1,
-                    title: 'Baked',
-                    imageUrl: 'http://tinyurl.com/y9oxkopq',
-                    linkUrl:'baked'
-                },
-                {
-                    id: 2,
-                    title: 'Cakes',
-                    imageUrl: 'http://tinyurl.com/yblo7ens',
-                    linkUrl:'cakes'
-                },
-                {
-                    id: 3,
-                    title: 'Cookies',
-                    imageUrl: 'http://tinyurl.com/y8wtdqkx',
-                    linkUrl:'cookies'
-                },
-                {
-                    id: 4,
-                    title: 'Brownie',
-                    imageUrl: 'http://tinyurl.com/y7ardlqo',
-                    size:'large',
-                    linkUrl:'brownie'
-                },
-                {
-                    id: 5,
-                    title: 'Cup Cakes',
-                    imageUrl: 'http://tinyurl.com/y9gn4f8l',
-                    size:'large',
-                    linkUrl:'cupcakes'
-                }
-            ]
+  constructor(props) {
+    super(props);
+    this.state = {
+      menus: [
+        {
+          id: 1,
+          title: "Baked",
+          imageUrl:
+            "https://img1.mashed.com/img/gallery/the-best-bakery-in-every-state/intro-1601499029.jpg",
+          linkUrl: "baked"
+        },
+        {
+          id: 2,
+          title: "Cakes",
+          imageUrl:
+            "https://wrightsdairyfarm.com/assets/img/dessertcakes/dessertcakebanner2.jpg",
+          linkUrl: "cakes"
+        },
+        {
+          id: 3,
+          title: "Cookies",
+          imageUrl:
+            "https://wrightsdairyfarm.com/assets/img/bakery/bakery-banner.jpg",
+          linkUrl: "cookies"
+        },
+        {
+          id: 4,
+          title: "Brownie",
+          imageUrl:
+            "https://wrightsdairyfarm.com/assets/img/bakery/blueberry.jpg",
+          size: "large",
+          linkUrl: "brownie"
+        },
+        {
+          id: 5,
+          title: "Cup Cakes",
+          imageUrl:
+            "https://wrightsdairyfarm.com/assets/img/cupcakes/cupcakebanner.jpg",
+          size: "large",
+          linkUrl: "cupcakes"
         }
-    }
-    render() {
-        return (
-            <div>
-                <Menu items={this.state.menus}/>
-            </div>
-        );
-    }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Menu items={this.state.menus} />
+      </div>
+    );
+  }
 }
 export default HomePage;
